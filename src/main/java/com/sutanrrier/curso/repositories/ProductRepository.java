5612c3e67d9fe1b0129c1724380792c24f8a715d
Sutanrrier <pabloryanbr0@gmail.com>
2022-06-08 13:33:02 -0300

    Entity Product

---
+++ src/main/java/com/sutanrrier/curso/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.sutanrrier.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sutanrrier.curso.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+	
+}
