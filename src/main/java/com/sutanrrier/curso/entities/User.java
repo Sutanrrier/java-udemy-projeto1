595eed1454de3e5fe8ddcc987f9c90da425c25e1
Sutanrrier <pabloryanbr0@gmail.com>
2022-06-08 16:46:14 -0300

    [200~User insert

--- src/main/java/com/sutanrrier/curso/entities/User.java
+++ src/main/java/com/sutanrrier/curso/entities/User.java
@@ -15,26 +15,27 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name="tb_user")
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
+
 	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
-	
-	//Construtores
+
+	// Construtores
 	public User() {
-		
+
 	}
+
 	public User(Integer id, String name, String email, String phone, String password) {
 		this.id = id;
 		this.name = name;
@@ -42,52 +43,58 @@ public class User implements Serializable {
 		this.phone = phone;
 		this.password = password;
 	}
-	
-	//Getters e Setters
+
+	// Getters e Setters
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public List<Order> getOrders() {
 		return orders;
 	}
-	
-	//Hashcode e Equals
+
+	// Hashcode e Equals
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -99,5 +106,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
