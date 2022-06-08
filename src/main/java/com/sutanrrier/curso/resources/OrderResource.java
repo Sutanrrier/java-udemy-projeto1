f95d672b2cddbaa11e3c46884e346dc877834b5d
Sutanrrier <pabloryanbr0@gmail.com>
2022-06-08 16:31:51 -0300

    Subtotal & Total methods

--- src/main/java/com/sutanrrier/curso/resources/OrderResource.java
+++ src/main/java/com/sutanrrier/curso/resources/OrderResource.java
@@ -25,7 +25,7 @@ public class OrderResource {
 	}
 	
 	@GetMapping(value="/{id}")
-	public ResponseEntity<Order> findByiD(@PathVariable Integer id){
+	public ResponseEntity<Order> findByiD(@PathVariable Long id){
 		Order obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
